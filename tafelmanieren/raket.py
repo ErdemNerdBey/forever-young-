5636bcd10ddc0b887df80145f5dbc16a5598a59f
Idioot <99065788@mydavinci.nl>
2021-09-14 12:54:22 +0200

    robot arm

---
+++ tafelmanieren/raket.py
@@ -0,0 +1,7 @@
+#opdracht 2
+import time
+
+for i in range(30 , 0 , -1):
+    print(i)
+    time.sleep(0.98)
+print('lanceeeeerrrrr!')
---
+++ tafelmanieren/tafel.py
@@ -0,0 +1,3 @@
+#opdracht 1
+for i in range(1 , 11):   
+    print(i * 4)
